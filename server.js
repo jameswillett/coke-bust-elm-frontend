18d2d020f7a441cc3f3133c043b9a85725abd112
James Willett <jms.wlltt@gmail.com>
2018-08-01 14:31:09 -0400

    i dunno man

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/js', express.static(`${__dirname}/build/static/js`));
-app.use('/css', express.static(`${__dirname}/build/static/css`));
+app.use('/build/js', express.static(`${__dirname}/build/static/js`));
+app.use('/build/css', express.static(`${__dirname}/build/static/css`));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/build/index.html`));
