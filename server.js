dc48bea40db7ce737c1fa495315984edf2cf6a59
James Willett <jms.wlltt@gmail.com>
2018-08-01 14:28:46 -0400

    static stuff

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use('/js', express.static(`${__dirname}/build/static/js`));
+app.use('/css', express.static(`${__dirname}/build/static/css`));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/build/index.html`));
 });
